9821077c0a2ffc87c35ae06d27becdbdf3fa61e6
Yasarozan <yasarozankaraman@hotmail.com>
2022-08-18 15:41:10 +0300

    4

--- FintechYazilim-YasarOzanKaraman/Geo/Rectangle.cs
+++ FintechYazilim-YasarOzanKaraman/Geo/Rectangle.cs
@@ -11,16 +11,23 @@ namespace FintechYazilim_YasarOzanKaraman
     class Rectangle:Shape
     {
         SolidBrush brusher = new SolidBrush(Color.Black);
+        PointF point1, point2, point3,point4;
 
         private int sx,sy,bx,by;
         Point StartXY;
         Point EndXY;    
         
         public override void Fill(Graphics graphics,SolidBrush brush,int x, int y, int cx, int cy)
-        {
-            bx= cx;  
+        {/*
+            point1 = new Point(Math.Abs(x - 2 * cx), Math.Abs(y - 2 * cy));
+            point2 = new Point(Math.Abs(x-cx), Math.Abs(y - 2 * cy));
+            point3 = new Point(Math.Abs(x-2*cx), y);   
+            point4 = new Point(x, y);
+            PointF[] curvePoints = { point1, point2, point3,point4 };
+            graphics.FillPolygon(brush, curvePoints);*/
+            bx = cx;  
             by= cy;
-            sx = Math.Abs(x - cx);
+            sx = Math.Abs(x -cx);
             sy= Math.Abs(y - cy);
             graphics.FillRectangle(brush,bx,by,sx,sy);
 
